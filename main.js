aa6651de8a9d022763e064a5346e6f4375a37a20
Zoe <mariagiargei031@gmail.com>
2022-05-18 00:09:00 -0300

    inicie index + 4 paginas, agregué estilos con sass, agruegué palabras claves, descripciones y titles

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    margin:10,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
